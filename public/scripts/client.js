633309d2fce3685b5a961bbabd71acc51434215a
Anne-Dionido <anne.dionido@telus.com>
2022-12-07 22:26:50 -0700

    commit updates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,127 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const renderTweets = function(tweets, outputElement) {
+    $(outputElement).empty();
+    $.each(tweets, (index, tweetObject) => $(outputElement)
+      .prepend(createTweetElement(tweetObject))
+    );
+  };
+
+  const createTweetElement  = (tweetObject) => {
+    const ageOutput = timeago.format(tweetObject.created_at);
+    const $userImage = $('<img>')
+    .addClass('user-image')  
+    .attr({
+      'src': tweetObject.user.avatars,
+      'alt': `Profile image for ${tweetObject.user.name}`
+    });
+    const $userName = $('<p>')
+      .addClass('user-name')
+      .text(tweetObject.user.name);
+    const $userHandle = $('<p>')
+      .addClass('user-handle')
+      .text(tweetObject.user.handle);
+    const $contentText = $('<p>')
+      .addClass('content-text')  
+      .text(tweetObject.content.text);
+    
+    const $article = 
+    `<article class="tweet">
+      <header>
+        ${$userImage.prop('outerHTML')}
+        ${$userName.prop('outerHTML')}
+        ${$userHandle.prop('outerHTML')}
+      </header>
+      ${$contentText.prop('outerHTML')}
+      <footer>
+        <p class="tweet-age">${ageOutput}</p>
+        <p class="actions">
+          <a href="#" title="Report tweet"><i class="fa-solid fa-flag"></i></a>
+          <a href="#" title="Retweet"><i class="fa-solid fa-retweet"></i></a>
+          <a href="#" title="Save tweet"><i class="fa-solid fa-heart"></i></a>
+        </p>
+      </footer>
+    </article>`
+
+    return $article;
+  };
+
+  const loadTweets = (outputElement) => {
+    $.ajax('/tweets', { method: 'GET' })
+    .then((tweets) => {
+      renderTweets(tweets, outputElement);
+    });
+  };
+
+  loadTweets('#tweets-container');
+
+  const validate = {
+    minLength: {
+      isValid: false,
+      message: null,
+      eval: function(input) {
+        if (!input) {
+          this.isValid = false;
+          this.message = 'Please say something in your tweet!';
+          return this.isValid;
+        }
+        this.isValid = true;
+        return this.isValid;
+      },
+    },
+    maxLength: {
+      isValid: false,
+      message: null,
+      eval: function(input, maxLength) {
+        if (input.length > maxLength) {
+          this.isValid = false;
+          this.message = `Tweets cannot exceed ${maxLength} characters`;
+          return this.isValid
+        }
+        this.isValid = true;
+        return this.isValid;
+      },
+    },
+    eval: function(input, maxLength) {
+      this.minLength.eval(input);
+      this.maxLength.eval(input, maxLength);
+    },
+  };
+
+  Object.defineProperty(validate, 'eval', { enumerable: false });
+  Object.defineProperty(validate.minLength, 'eval', { enumerable: false });
+  Object.defineProperty(validate.maxLength, 'eval', { enumerable: false });
+
+  const $newTweetForm = $('#new-tweet');
+  $newTweetForm.on('submit', (event) => {
+    event.preventDefault();
+    const tweetData = $newTweetForm.serialize();
+    const $tweetInputElement = ('#tweet-text');
+    const tweetInputValue = $($tweetInputElement).val();
+    const $errorElement = $("<div>").addClass('error');
+    validate.eval(tweetInputValue, 140);
+    $('.error').remove();
+    if (validate.minLength.isValid === false) {
+      $errorElement.text(validate.minLength.message)
+        .insertBefore($tweetInputElement)
+        .hide()
+        .show('slow');
+      return;
+    }
+    if (validate.maxLength.isValid === false) {
+      $errorElement.text(validate.maxLength.message)
+        .insertBefore($tweetInputElement)
+        .hide()
+        .show('slow');
+      return;
+    }
+    $.post('/tweets', tweetData, (response) => {
+      $newTweetForm[0].reset();
+      loadTweets('#tweets-container');
+    });
+  });
+
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,36 @@
+$(() => {
+  const handleEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  };
+
+  const getCounterText = (maxTweetLength, currentTweetLength) => {
+    const counterValue = maxTweetLength - currentTweetLength;
+    return counterValue;
+  }
+
+  const isTweetLengthExceeded = (maxTweetLength, currentTweetLength) => {
+    isExceeded = (maxTweetLength - currentTweetLength) < 0;
+    return isExceeded;
+  }
+
+  const tweetInputElement = $('#tweet-text');
+  const tweetInputParent = $(tweetInputElement).parent();
+  const counterElement = $(tweetInputParent).find('.counter');
+  const maxTweetLength = 140;
+  $(counterElement).text(maxTweetLength);
+  $(tweetInputElement).on('keyup', function(event) {
+    handleEnterKey(event);
+    const currentLength = this.value.length;
+    const counterText = getCounterText(maxTweetLength, currentLength);
+    $(counterElement).text(counterText);
+    if (isTweetLengthExceeded(maxTweetLength, currentLength)) {
+      $(counterElement).removeClass('danger').addClass('danger');
+    } else {
+      $(counterElement).removeClass('danger');
+    }
+
+  })
+
+});
